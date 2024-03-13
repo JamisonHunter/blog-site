289f07c097b6e967e3b0794da9a7648c7c6c3d60
Jamison Hunter <hunterjamison6@gmail.com>
2024-03-13 18:03:52 -0400

    Implemented Routing
    The sire now has two page routes. Due to difficulties with the current react-router-dom version, I am using react-router-dom version 5.

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,13 @@
+// About.jsx
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page of the blog site.</p>
+    </div>
+  );
+}
+
+export default About;
